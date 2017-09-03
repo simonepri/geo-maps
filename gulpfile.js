ca5e4f938832b00199390eabe2da6759bc211f3d
simonepri <simone.pri@hotmail.it>
2017-09-04 01:00:18 +0200

    Fix compress script

--- gulpfile.js
+++ gulpfile.js
@@ -26,15 +26,16 @@ gulp.task('save', () => {
  *  Compress the political boundaries of the entire world.
  */
 gulp.task('compress', async () => {
-  const world = require(path.join(GEO_DIR, '100', 'world.geo.json'));
+  const worldDir = path.join(GEO_DIR, '100', 'world.geo.json');
   const sizes = ['0.25', '0.5', '0.75', '1', '1.5', '2', '2.5', '3', '3.5', '4', '4.5', '5', '6', '7', '8', '9', '10', '15', '20', '25', '50', '75'];
   const bar = new ProgressBar({total: sizes.length});
 
   for (const size of sizes) {
     const saveDir = path.join(GEO_DIR, size, 'world.geo.json');
+    const cmd = '-i ' + worldDir + ' -simplify ' + size + '% -o format=geojson ' + saveDir;
+
     // eslint-disable-next-line no-await-in-loop
-    await pify(mapshaper.applyCommands)('-simplify dp ' + size + '%', world)
-      .then(compressed => fs.outputJson(saveDir, compressed));
+    await pify(mapshaper.runCommands)(cmd);
     bar.tick();
   }
 });
