1f8e683769eee65ca249d7e95d90222ecb4ca47c
chqma <chq.matteo@gmail.com>
2017-09-03 11:14:42 +0200

    Add search function

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 'use strict';
 
-module.exports = {};
+const PolygonLookup = require('polygon-lookup');
+const worldGeojson = require('./data/world.json');
+const worldLookup = new PolygonLookup(worldGeojson);
+
+
+/**
+ * @param {LatLng} latlng
+ * @returns ISO 3166 alpha-3 country code for geographic coordinates
+ */
+module.exports = function (latlng) {
+  return worldLookup.search(latlng.lng, latlng.lat);
+};
