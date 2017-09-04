0a4ed00054a9f033d771612dceff87d3a3532a03
simonepri <simone.pri@hotmail.it>
2017-09-04 10:25:04 +0200

    Update export settings

--- gulpfile.js
+++ gulpfile.js
@@ -15,25 +15,36 @@ const GEO_DIR = path.join(__dirname, 'geojson');
 /**
  *  Save the political boundaries of the entire world.
  */
-gulp.task('save', () => {
-  const saveDir = path.join(GEO_DIR, '100', 'world.geo.json');
+gulp.task('save', async () => {
+  const saveDir = path.join(GEO_DIR, 'orig');
+  const savePath = path.join(saveDir, 'world.geo.json');
 
-  return utils.getMap.getWorld()
-    .then(world => fs.outputJson(saveDir, world));
+  await fs.ensureDir(saveDir);
+  await utils.getMap.getWorld()
+    .then(world => fs.outputJson(savePath, world));
 });
 
 /**
  *  Compress the political boundaries of the entire world.
  */
 gulp.task('compress', async () => {
-  const worldDir = path.join(GEO_DIR, '100', 'world.geo.json');
-  const sizes = ['0.25', '0.5', '0.75', '1', '1.5', '2', '2.5', '3', '3.5', '4', '4.5', '5', '6', '7', '8', '9', '10', '15', '20', '25', '50', '75'];
+  const worldPath = path.join(GEO_DIR, 'orig', 'world.geo.json');
+  const sizes = [
+    '1', '2', '5',
+    '10', '25', '50',
+    '100', '250', '500',
+    '1000', '2500', '5000',
+    '10000', '25000', '50000',
+    '100000', '250000', '500000'
+  ];
   const bar = new ProgressBar({total: sizes.length});
 
   for (const size of sizes) {
-    const saveDir = path.join(GEO_DIR, size, 'world.geo.json');
-    const cmd = '-i ' + worldDir + ' -simplify ' + size + '% -o format=geojson ' + saveDir;
-
+    const saveDir = path.join(GEO_DIR, size + 'm');
+    const savePath = path.join(saveDir, 'world.geo.json');
+    const cmd = '-i ' + worldPath + ' -simplify keep-shapes interval=' + size + ' -o format=geojson ' + savePath;
+    // eslint-disable-next-line no-await-in-loop
+    await fs.ensureDir(saveDir);
     // eslint-disable-next-line no-await-in-loop
     await pify(mapshaper.runCommands)(cmd);
     bar.tick();
