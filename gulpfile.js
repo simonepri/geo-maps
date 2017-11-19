8efb5e4dd13f6691e8cd0bf342da39076e1fe235
simonepri <simone.pri@hotmail.it>
2017-11-19 19:07:49 +0100

    Update build script

--- gulpfile.js
+++ gulpfile.js
@@ -161,7 +161,7 @@ gulp.task('compress', async () => {
       const mapPath = path.join(tmpDir, map + '.geo.json');
       const outMapDir = path.join(buildDir, map, size);
       const outMapPath = path.join(outMapDir, 'map.geo.json');
-      const cmd = '-i ' + mapPath + ' -simplify keep-shapes interval=' + sizes[size] + ' -clean -o precision=' + precision[size] + ' format=geojson bbox prettify ' + outMapPath;
+      const cmd = '-i ' + mapPath + ' -simplify keep-shapes interval=' + sizes[size] + ' -clean -o precision=' + precision[size] + ' format=geojson ' + outMapPath;
 
       // eslint-disable-next-line no-await-in-loop
       await fs.ensureDir(outMapDir);
